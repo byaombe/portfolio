59c00ff96fc23d1a67db4712e0d74d949e6eb314
Byaombe <34215129+byaombe@users.noreply.github.com>
2024-12-07 23:55:55 -0600

    worked on the header-container, and added the nav bar on top right

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portfolio
+In this project, I'l create my portfolio using HTML, css, and JS.
---
+++ script.js
@@ -0,0 +1,7 @@
+
+//scroll to portfolio 
+const scrollPortfolio = () =>  document.getElementById("portfolio").scrollIntoView({behavior: "smooth" });
+
+//scroll to services 
+//scroll to contact 
+//scroll to footer 
